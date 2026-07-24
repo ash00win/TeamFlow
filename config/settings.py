d707f9d20145ca8f2541c53874f3074c3a77f617
Ashwin <ashwinjoykuttikkadan@gmail.com>
2026-07-24 23:38:54 +0530

    Serve static files correctly when DEBUG=False via whitenoise
    Django stops serving static files itself once DEBUG=False, which would
    have left the Admin panel and DRF browsable API completely unstyled
    in production. Add whitenoise middleware + compressed manifest storage,
    run collectstatic in the Docker start command, and bind gunicorn to
    the platform-provided $PORT instead of a hardcoded 8000.

--- config/settings.py
+++ config/settings.py
@@ -70,6 +70,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -168,6 +169,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage',
+    },
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    },
+}
 
 AUTH_USER_MODEL = 'accounts.User'
 
